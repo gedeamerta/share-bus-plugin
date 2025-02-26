e08c605318036a0ec20599cca6a8af6469476946
komangss <komanggt12@gmail.com>
2025-02-26 20:36:51 +1100

    finalised update for plugin

--- index.php
+++ index.php
@@ -9,6 +9,9 @@ Author: Smartmates
 if (!defined('ABSPATH'))
     exit; // Prevent direct access
 
+/*
+================================== LIVE START ===================================
+*/
 // Shortcode for trips date page
 function zc_trips_date_shortcode()
 {
@@ -22,13 +25,21 @@ add_shortcode('zc_trips_date', 'zc_trips_date_shortcode');
 // Shortcode for trip details page
 function zc_trip_details_shortcode()
 {
-    wp_enqueue_style('zc-crud-style', plugins_url('assets/css/trip-details-pages1.css', __FILE__));
+    wp_enqueue_style('zc-crud-style', plugins_url('assets/css/trip-details-pages2.css', __FILE__));
     ob_start();
     include plugin_dir_path(__FILE__) . 'templates/trip-details-page.php';
     return ob_get_clean();
 }
 add_shortcode('zc_trip_details', 'zc_trip_details_shortcode');
 
+
+/*
+================================== LIVE END ===================================
+*/
+
+/*
+================================== TEST START ===================================
+*/
 function zc_trips_date_test_shortcode()
 {
     wp_enqueue_style('zc-crud-style', plugins_url('test/assets/css/trip-dates-page1.css', __FILE__));
@@ -41,9 +52,12 @@ add_shortcode('zc_trips_date_test', 'zc_trips_date_test_shortcode');
 // Shortcode for trip details page
 function zc_trip_details_test_shortcode()
 {
-    wp_enqueue_style('zc-crud-style', plugins_url('test/assets/css/trip-details-page1.css', __FILE__));
+    wp_enqueue_style('zc-crud-style', plugins_url('test/assets/css/trip-details-page3.css', __FILE__));
     ob_start();
     include plugin_dir_path(__FILE__) . 'test/trip-details-page.php';
     return ob_get_clean();
 }
 add_shortcode('zc_trip_details_test', 'zc_trip_details_test_shortcode');
+/*
+================================== TEST END ===================================
+*/
